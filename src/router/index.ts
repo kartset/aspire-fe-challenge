6836fef728a97462e7deec75ea020eea2048e866
Kartik Setia <kartset10@gmail.com>
2023-10-15 23:56:58 +0530

    added a currenthomeview file to export current homeview

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import DesktopHomeView from '../views/DesktopHomeView.vue'
-import { isMobile } from '@/utils/helper'
+import  CurrentHomeView from '../views/CurrentHomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: isMobile() ? HomeView : DesktopHomeView
+      component: CurrentHomeView
     },
   ]
 })
---
+++ src/views/CurrentHomeView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-if="type == 'xs'" >
+        <MobileHomeView />
+    </div>
+    <div v-else-if="type=='lg'" >
+        <WebHomeView />
+    </div>
+    <div v-else >
+        No Show. Please Either increase or decrease with
+    </div>
+</template>
+
+<script setup lang="ts">
+import  MobileHomeView from './MobileHomeView.vue';
+import  WebHomeView from './WebHomeView.vue';
+import { useBreakpoints } from '../utils/composables/useBreakpoints'
+
+const { type } = useBreakpoints()
+
+
+
+</script>
+
+<style scoped>
+
+</style>
